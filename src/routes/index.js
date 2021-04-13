7aa87fe9602bd4af49b1f71b682f99ac77331cc4
twicks95 <teguh.wck95@gmail.com>
2021-04-14 05:41:37 +0700

    Modify routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const Route = express.Router()
+const route = express.Router()
 
 const movieRouter = require('../modules/movie/movie_routes')
 const premiere = require('../modules/premiere/premiere_routes')
-// const schedule = require('../modules/schedule/schedule_routes')
+const schedule = require('../modules/schedule/schedule_routes')
 const booking = require('../modules/booking/booking_routes')
 
-Route.use('/movie', movieRouter)
-Route.use('/premiere', premiere)
-// Route.use('/schedule', schedule)
-Route.use('/booking', booking)
+route.use('/movie', movieRouter)
+route.use('/premiere', premiere)
+route.use('/schedule', schedule)
+route.use('/booking', booking)
 
-module.exports = Route
+module.exports = route
