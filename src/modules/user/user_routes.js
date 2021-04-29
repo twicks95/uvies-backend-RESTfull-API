2dc70f3459add6e941f4700b78d935f476cebf39
twicks95 <teguh.wck95@gmail.com>
2021-04-30 06:53:10 +0700

    Add auth and upload middleware

--- src/modules/user/user_routes.js
+++ src/modules/user/user_routes.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const route = express.Router()
+
+const authMiddleware = require('../../middleware/auth')
+const uploadFile = require('../../middleware/uploads')
+
 const userModel = require('./user_model')
 
-route.patch('/:id', userModel.updateUser)
-route.delete('/:id', userModel.deleteUser)
+route.patch(
+  '/:id',
+  authMiddleware.authentication,
+  uploadFile,
+  userModel.updateUser
+)
+route.delete('/:id', authMiddleware.authentication, userModel.deleteUser)
 
 module.exports = route
