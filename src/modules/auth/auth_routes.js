8e1664b6945d1d3937da098695bc3bd6f4be8e0c
twicks95 <teguh.wck95@gmail.com>
2021-04-29 16:43:18 +0700

    Add delete user feature

--- src/modules/auth/auth_routes.js
+++ src/modules/auth/auth_routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const route = express.Router()
-const { register, login, updateUser } = require('./auth_controller')
+const { register, login, updateUser, deleteUser } = require('./auth_controller')
 
 route.post('/register', register)
 route.post('/login', login)
 route.patch('/:id', updateUser)
+route.delete('/:id', deleteUser)
 
 module.exports = route
