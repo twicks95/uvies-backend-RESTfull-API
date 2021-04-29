c7a0bbfe1deb4b2d78b253444f97321fc01764d0
twicks95 <teguh.wck95@gmail.com>
2021-04-29 21:26:02 +0700

    Add endpoint for update and delete user data

--- src/modules/user/user_routes.js
+++ src/modules/user/user_routes.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const route = express.Router()
+const userModel = require('./user_model')
 
-route.patch('/:id', user)
+route.patch('/:id', userModel.updateUser)
+route.delete('/:id', userModel.deleteUser)
+
+module.exports = route
