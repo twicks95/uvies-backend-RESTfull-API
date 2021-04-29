3be3253314190827d1a83e84a7084c4b8a28c594
twicks95 <teguh.wck95@gmail.com>
2021-04-29 21:22:37 +0700

    Move updateUser and deleteUser model to user_model

--- src/modules/auth/auth_model.js
+++ src/modules/auth/auth_model.js
@@ -23,33 +23,5 @@ module.exports = {
         !error ? resolve(result) : reject(new Error(error))
       })
     })
-  },
-
-  updateUser: (setData, id) => {
-    return new Promise((resolve, reject) => {
-      db.query(
-        'UPDATE user SET ? WHERE user_id = ?',
-        [setData, id],
-        (error, result) => {
-          if (!error) {
-            const newResult = {
-              id,
-              ...setData
-            }
-            resolve(newResult)
-          } else {
-            reject(new Error(error))
-          }
-        }
-      )
-    })
-  },
-
-  deleteUser: (id) => {
-    return new Promise((resolve, reject) => {
-      db.query('DELETE FROM user WHERE user_id = ?', id, (error, result) => {
-        !error ? resolve(result) : reject(new Error(error))
-      })
-    })
   }
 }
