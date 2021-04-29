3006d32a31c179bbb2ca8fae637713216058098b
twicks95 <teguh.wck95@gmail.com>
2021-04-29 21:20:06 +0700

    Move updateUser and deleteUser to user_controller

--- src/modules/auth/auth_controller.js
+++ src/modules/auth/auth_controller.js
@@ -77,56 +77,5 @@ module.exports = {
     } catch (error) {
       return wrapper.response(res, 400, 'Bad Request', error)
     }
-  },
-
-  updateUser: async (req, res) => {
-    try {
-      const { id } = req.params
-      const {
-        firstName,
-        lastName,
-        email,
-        phoneNumber,
-        changePassword
-      } = req.body
-
-      let setData
-      if (!changePassword) {
-        setData = {
-          user_name: `${firstName} ${lastName}`,
-          user_email: email,
-          user_phone_number: phoneNumber,
-          user_updated_at: new Date(Date.now())
-        }
-      } else {
-        // password encrypting process
-        const salt = bcrypt.genSaltSync(10)
-        const encryptedPassword = bcrypt.hashSync(changePassword, salt)
-
-        setData = {
-          user_name: `${firstName} ${lastName}`,
-          user_email: email,
-          user_password: encryptedPassword,
-          user_phone_number: phoneNumber,
-          user_updated_at: new Date(Date.now())
-        }
-      }
-
-      const result = await authModel.updateUser(setData, id)
-      delete result.user_password
-      return wrapper.response(res, 200, 'Success Update User Data', result)
-    } catch (error) {
-      return wrapper.response(res, 400, 'Bad Request', error)
-    }
-  },
-
-  deleteUser: async (req, res) => {
-    try {
-      const { id } = req.params
-      const result = await authModel.deleteUser(id)
-      return wrapper.response(res, 200, 'Success Delete User', result)
-    } catch (error) {
-      return wrapper.response(res, 400, 'Bad Request', error)
-    }
   }
 }
