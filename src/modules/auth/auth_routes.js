0ca378f0bed31f7999a997df7a6c9c665902bfb0
twicks95 <teguh.wck95@gmail.com>
2021-05-02 22:19:53 +0700

    Update auth_routes

--- src/modules/auth/auth_routes.js
+++ src/modules/auth/auth_routes.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const route = express.Router()
-const { register, login, updateUser, deleteUser } = require('./auth_controller')
+const { register, login } = require('./auth_controller')
 
 route.post('/register', register)
 route.post('/login', login)
-route.patch('/:id', updateUser)
-route.delete('/:id', deleteUser)
 
 module.exports = route
