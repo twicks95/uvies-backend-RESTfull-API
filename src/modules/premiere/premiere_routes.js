0aa2834c6fa40f8bc9a22685dc474abb1d292e57
twicks95 <teguh.wck95@gmail.com>
2021-05-02 21:31:34 +0700

    Add auth middleware

--- src/modules/premiere/premiere_routes.js
+++ src/modules/premiere/premiere_routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const route = express.Router()
+
+const authMiddleware = require('../../middleware/auth')
+
 const premiereController = require('./premiere_controller')
 
 route.get('/', premiereController.getAllPremiere)
-route.post('/', premiereController.postPremiere)
-route.patch('/:id', premiereController.updatePremiere)
-route.delete('/:id', premiereController.deletePremiere)
+route.get('/:id', premiereController.getAllPremiere)
+route.post('/', authMiddleware.isAdmin, premiereController.postPremiere)
+route.patch('/:id', authMiddleware.isAdmin, premiereController.updatePremiere)
+route.delete('/:id', authMiddleware.isAdmin, premiereController.deletePremiere)
 
 module.exports = route
