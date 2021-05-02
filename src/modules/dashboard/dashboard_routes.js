5a430b054c4ec2648aae211848c12f9f1b30b692
twicks95 <teguh.wck95@gmail.com>
2021-05-02 22:15:33 +0700

    Add authMiddleware.authentication

--- src/modules/dashboard/dashboard_routes.js
+++ src/modules/dashboard/dashboard_routes.js
@@ -5,6 +5,11 @@ const authMiddleware = require('../../middleware/auth')
 
 const dashboardController = require('./dashboard_controller')
 
-route.get('/', authMiddleware.isAdmin, dashboardController.getAllEarnings)
+route.get(
+  '/',
+  authMiddleware.authentication,
+  authMiddleware.isAdmin,
+  dashboardController.getAllEarnings
+)
 
 module.exports = route
