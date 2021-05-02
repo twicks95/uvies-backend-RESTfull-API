66e3bbae32172b7f2eb56510e657b6b8e0a997df
twicks95 <teguh.wck95@gmail.com>
2021-05-02 13:28:42 +0700

    Update helper pagination

--- src/helpers/pagination.js
+++ src/helpers/pagination.js
@@ -1,11 +1,11 @@
 module.exports = {
-  pageInfo: (page, totalPage, limit, totalData) => {
+  pageInfo: (page, totalPage, limit, totalData, offset) => {
     const pageInfo = {}
     pageInfo.page = page
     pageInfo.totalPage = totalPage
     pageInfo.limit = limit
     pageInfo.totalData = totalData
-
+    pageInfo.offset = offset
     return pageInfo
   }
 }
