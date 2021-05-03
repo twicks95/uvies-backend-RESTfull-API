606c2fcdf10b82fa307032249d2efcafb8a4f707
twicks95 <teguh.wck95@gmail.com>
2021-05-03 12:38:40 +0700

    Use env variable at database name

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'uvies_app'
+  database: process.env.DB_NAME
 })
 
 db.connect((error) => {
