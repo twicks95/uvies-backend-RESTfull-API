3fdc77dd590cd30f0d3f099180b54aace6a5da0b
twicks95 <teguh.wck95@gmail.com>
2021-07-19 21:21:30 +0700

    update mysql & add redis config

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql2')
 require('dotenv').config()
+const mysql = require('mysql2')
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 })
 
 db.connect((error) => {
