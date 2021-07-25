63ea9fd19fb6e8d66d29744f7ac4ddda642ece61
twicks95 <teguh.wck95@gmail.com>
2021-07-25 10:28:52 +0700

    Modify wrapper helper

--- src/helpers/wrapper.js
+++ src/helpers/wrapper.js
@@ -1,11 +1,11 @@
 module.exports = {
   response: (response, status, msg, data, pagination) => {
-    const result = {}
-    result.status = status || 200
-    result.msg = msg
-    result.data = data
-    result.pagination = pagination
-
+    const result = {
+      status: status || 200,
+      msg,
+      data,
+      pagination
+    }
     return response.status(result.status).json(result)
   }
 }
