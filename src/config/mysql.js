876569ef5cae6f1463f3632ceb87b67bbec503ff
twicks95 <teguh.wck95@gmail.com>
2021-08-09 19:53:42 +0700

    Change db setup connection

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2')
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  database: process.env.DB_NAME
 })
 
 db.connect((error) => {
